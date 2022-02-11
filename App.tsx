ca9173b9c6ac6a93fe7f515f66979e84d8737542
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2022-02-11 18:58:54 +0530

    Stream Chat SDK setup

--- App.tsx
+++ App.tsx
@@ -1,22 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { StreamChat } from 'stream-chat'
+import { Chat, OverlayProvider, ChannelList } from 'stream-chat-expo'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+import { API_KEY } from '@env'
+
+const client = StreamChat.getInstance(API_KEY)
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
+
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: 'Victor',
+          name: 'AkuChukwu',
+          image: 'https://i.imgur.com/fR9Jz14.png',
+        },
+        client.devToken('Victor'),
+      )
+
+      const channel = client.channel('messaging', 'ChatApp', {
+        name: 'ChatApp.dev',
+      })
+      await channel.watch()
+    }
+
+    connectUser()
+
+    return () => client.disconnectUser()
+  }, [])
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider>
+          <Chat client={client}>
+            {/* <Navigation colorScheme={colorScheme} /> */}
+            <ChannelList />
+          </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
